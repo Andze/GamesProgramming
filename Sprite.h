de3915910daaa3ea85d9c46261e3434213df0a37
name <email>
2016-02-15 15:19:41 +0000

    Sprite Class

---
+++ Sprite.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef Sprite_h
+#define Sprite_h
+
+#include "common.h"
+
+#endif /* end of include guard: */
+class Sprite
+{
+public:
+	SDL_Rect rectangle;
+	Sprite(int x, int y, int w, int h);
+	Sprite(const Sprite& sprite);
+	~Sprite();
+};
+
---
+++ common.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <memory>
+#include <SDL.h>
